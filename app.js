730db4c8f1b2539f5b05a6f265a9caa5070967e3
Caroline Panggabean <55356429+olinexs@users.noreply.github.com>
2023-07-12 15:42:48 +0700

    app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.static(path.join(__dirname, '')))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, '') })
+    res.sendFile('ngetes.html', { root: path.join(__dirname, '') })
 })
 
 app.listen(PORT, HOST)
